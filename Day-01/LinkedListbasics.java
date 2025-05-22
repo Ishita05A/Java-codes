ccfa3c66340724e31fbf9987c09f346562384cb9
Ishita05A <ishita05agarwal@gmail.com>
2025-05-22 20:05:45 +0530

    Basics of LinkedList

---
+++ Day-01/LinkedListbasics.java
@@ -0,0 +1,33 @@
+public class LinkedListbasics {
+    public static class Node{
+        int data;
+        Node next;
+        Node(int data){
+            this.data=data;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node a=new Node(3);
+        Node b=new Node(4);
+        Node c=new Node(5);
+        Node d=new Node(6);
+        a.next=b;
+        b.next=c;
+        c.next=d;
+        System.out.println(a.data);
+        System.out.println(a.next.data);
+        System.out.println(a.next.next.data);
+        Node temp =a;
+        for(int i=1;i<=4;i++){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+        System.out.println();
+        temp=a;
+        while(temp!=null){
+            System.out.print(temp.data+" ");
+            temp=temp.next;
+        }
+    }
+}
