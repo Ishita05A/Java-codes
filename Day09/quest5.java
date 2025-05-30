109d1f3cf13264ec54faca64bd33d57afe86d568
Ishita05A <ishita05agarwal@gmail.com>
2025-05-31 03:26:27 +0530

    updated code

---
+++ Day09/quest5.java
@@ -0,0 +1,28 @@
+package Day09;
+
+import java.util.Scanner;
+
+public class quest5 {
+    static int noOfOccurance(int[] arr, int x) {
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no of element");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(noOfOccurance(arr, 3));
+        sc.close();
+    }
+
+}
