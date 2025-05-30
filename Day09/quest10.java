ea54ca31ff834aa3a0e7c68ce0110b757295e3d9
Ishita05A <ishita05agarwal@gmail.com>
2025-05-31 03:53:10 +0530

    updated code

---
+++ Day09/quest10.java
@@ -0,0 +1,30 @@
+package Day09;
+
+import java.util.Scanner;
+
+public class quest10 {
+    static int targetsum(int[] arr, int x) {
+        int count = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] + arr[j] == x)
+                    count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no of element");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(targetsum(arr, 7));
+        sc.close();
+
+    }
+
+}
---
+++ Day09/quest11.java
@@ -0,0 +1,31 @@
+package Day09;
+
+import java.util.Scanner;
+
+public class quest11 {
+    static int targetsum(int[] arr, int x) {
+        int count = 0;
+        for (int i = 0; i < arr.length - 2; i++) {
+            for (int j = i + 1; j < arr.length - 1; j++) {
+                for (int k = j + 1; k < arr.length; k++) {
+                    if (arr[i] + arr[j] + arr[k] == x)
+                        count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no of element");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(targetsum(arr, 12));
+        sc.close();
+    }
+
+}
