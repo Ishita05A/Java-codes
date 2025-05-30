8daa8f4386dc512e4dfabe5e244a7ce19eb879bf
Ishita05A <ishita05agarwal@gmail.com>
2025-05-31 04:23:40 +0530

    updated code

---
+++ Day09/quest15.java
@@ -0,0 +1,14 @@
+package Day09;
+
+public class quest15 {
+    public static void main(String[] args) {
+        int a = 23;
+        int b = 12;
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println(a);
+        System.out.println(b);
+    }
+
+}
