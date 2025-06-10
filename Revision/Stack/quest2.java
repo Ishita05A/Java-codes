49301809e4255e942e48981b70d6ceb07a56a28b
Ishita05A <ishita05agarwal@gmail.com>
2025-06-10 19:23:12 +0530

    updated code

---
+++ Revision/Stack/quest2.java
@@ -0,0 +1,36 @@
+// Balanced Bracket
+package Revision.Stack;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class quest2 {
+    static boolean isBalanced(String str) {
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '(') {
+                st.push(ch);
+            } else {
+
+                if (st.size() == 0)
+                    return false;
+                else
+                    st.pop();
+            }
+        }
+        if (st.size() == 0)
+            return true;
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter String ");
+        String str = sc.nextLine();
+        System.out.println(isBalanced(str));
+        sc.close();
+
+    }
+
+}
