d7c349650ae4cb27dda28ee60b4bc538be409f29
Ishita05A <ishita05agarwal@gmail.com>
2025-06-16 21:41:38 +0530

    updated code

---
+++ Day24/selectionSort.java
@@ -0,0 +1,43 @@
+package Day24;
+
+import java.util.Scanner;
+
+public class selectionSort {
+    static void display(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static void selectionsort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int min_idx = i;
+
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[min_idx]) {
+                    min_idx = j;
+                }
+            }
+            int temp = arr[min_idx];
+            arr[min_idx] = arr[i];
+            arr[i] = temp;
+
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of elements");
+        int n = sc.nextInt();
+        System.out.println("Enter Elements");
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        selectionsort(arr);
+        display(arr);
+        sc.close();
+
+    }
+
+}
