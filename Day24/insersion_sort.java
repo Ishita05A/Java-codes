10528bd63fb7aa2cc6120ca8debf48a3aa7210be
Ishita05A <ishita05agarwal@gmail.com>
2025-06-16 22:42:55 +0530

    updated code

---
+++ Day24/insersion_sort.java
@@ -0,0 +1,39 @@
+package Day24;
+
+import java.util.Scanner;
+
+public class insersion_sort {
+    static void display(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static void insersionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int j = i;
+            while (j > 0 && arr[j] < arr[j - 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j - 1];
+                arr[j - 1] = temp;
+                j--;
+
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of elements");
+        int n = sc.nextInt();
+        System.out.println("Enter Elements");
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        insersionSort(arr);
+        display(arr);
+        sc.close();
+    }
+
+}
