7fbaed8dd0c0cdb8e47f546d5b221049fc7faa6a
Ishita05A <ishita05agarwal@gmail.com>
2025-06-16 19:52:59 +0530

    updated code

---
+++ Day24/bubbleSort.java
@@ -0,0 +1,38 @@
+package Day24;
+
+import java.util.Scanner;
+
+public class bubbleSort {
+    static void display(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static void bubblesort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of elements");
+        int n = sc.nextInt();
+        System.out.println("Enter Elements");
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        bubblesort(arr);
+        display(arr);
+        sc.close();
+    }
+
+}
