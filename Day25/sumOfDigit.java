dfd9cd45d96ced3b81c3654964d03e46ff055975
Ishita05A <ishita05agarwal@gmail.com>
2025-06-18 04:38:49 +0530

    updated code

---
+++ Day25/sumOfDigit.java
@@ -0,0 +1,20 @@
+package Day25;
+
+import java.util.Scanner;
+
+public class sumOfDigit {
+    static int digitsum(int n) {
+        if (n < 10 && n > 0)
+            return n;
+        return (n % 10 + digitsum(n / 10));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int n = sc.nextInt();
+        System.out.println(digitsum(n));
+        sc.close();
+    }
+
+}
