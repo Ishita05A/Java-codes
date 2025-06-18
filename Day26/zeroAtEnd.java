83998c30c922c41465b0d024a2a55f500126dea7
Ishita05A <ishita05agarwal@gmail.com>
2025-06-18 18:57:38 +0530

    updated code

---
+++ Day26/zeroAtEnd.java
@@ -0,0 +1,38 @@
+package Day26;
+
+import java.util.Scanner;
+
+public class zeroAtEnd {
+    static void display(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static void zeroSorting(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] == 0 && arr[j + 1] != 0) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of elements");
+        int n = sc.nextInt();
+        System.out.println("Enter Elements");
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        zeroSorting(arr);
+        display(arr);
+        sc.close();
+    }
+
+}
