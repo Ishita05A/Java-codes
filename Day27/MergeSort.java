ed2ccc5646f49f21f609d4a4b66e9f487127fb59
Ishita05A <ishita05agarwal@gmail.com>
2025-06-19 17:55:56 +0530

    updated code

---
+++ Day27/MergeSort.java
@@ -0,0 +1,61 @@
+package Day27;
+
+import java.util.Scanner;
+
+public class MergeSort {
+    static void display(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static void merge(int[] arr, int r, int l, int mid) {
+        int n1 = mid - l + 1;
+        int n2 = r - mid;
+        int[] left = new int[n1];
+        int[] right = new int[n2];
+        for (int i = 0; i < n1; i++) {
+            left[i] = arr[l + i];
+        }
+        for (int j = 0; j < n2; j++)
+            right[j] = arr[mid + j + 1];
+        int i = 0;
+        int j = 0;
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (left[i] < right[j])
+                arr[k++] = left[i++];
+            else
+                arr[k++] = right[j++];
+        }
+        while (i < n1)
+            arr[k++] = left[i++];
+        while (j < n2)
+            arr[k++] = right[j++];
+    }
+
+    static void mergesort(int[] arr, int l, int r) {
+        if (l >= r)
+            return;
+        int mid = (l + r) / 2;
+        mergesort(arr, l, mid);
+        mergesort(arr, mid + 1, r);
+        merge(arr, r, l, mid);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of elements");
+        int n = sc.nextInt();
+        System.out.println("Enter Elements");
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        mergesort(arr, 0, n - 1);
+        display(arr);
+        sc.close();
+    }
+
+}
