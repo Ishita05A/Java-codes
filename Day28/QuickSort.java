179d982cc125f99f5ddca7d87bd684914d2f1462
Ishita05A <ishita05agarwal@gmail.com>
2025-06-21 01:48:23 +0530

    updated code

---
+++ Day28/QuickSort.java
@@ -0,0 +1,67 @@
+package Day28;
+
+import java.util.Scanner;
+
+public class QuickSort {
+    static void display(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static int partition(int[] arr, int st, int end) {
+        int pivot = arr[st];
+        int count = 0;
+        for (int i = st + 1; i <= end; i++) {
+            if (arr[i] <= pivot)
+                count++;
+        }
+        int pi = count + st;
+        swap(arr, st, pi);
+        int i = st;
+        int j = end;
+        while (i < pi && j > pi) {
+            if (arr[i] <= arr[pi])
+                i++;
+            if (arr[j] > arr[pi])
+                j--;
+            if (arr[i] > arr[pi] && arr[j] < arr[pi]) {
+                swap(arr, i, j);
+                i++;
+                j--;
+            }
+
+        }
+        return pi;
+    }
+
+    static void quick_sort(int[] arr, int st, int end) {
+        if (st >= end)
+            return;
+        int pi = partition(arr, st, end);
+        quick_sort(arr, st, pi - 1);
+        quick_sort(arr, pi + 1, end);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of elements");
+        int n = sc.nextInt();
+        System.out.println("Enter Elements");
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        quick_sort(arr, 0, n - 1);
+        display(arr);
+        sc.close();
+
+    }
+
+}
