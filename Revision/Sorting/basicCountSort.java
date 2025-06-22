cc0fef54f0b62ccffc6986b8959901a29a637476
Ishita05A <ishita05agarwal@gmail.com>
2025-06-22 18:53:33 +0530

    updated code

---
+++ Revision/Sorting/basicCountSort.java
@@ -0,0 +1,49 @@
+package Revision.Sorting;
+
+import java.util.Scanner;
+
+public class basicCountSort {
+    static void display(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static int findMax(int[] arr) {
+        int max = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] > max)
+                max = arr[i];
+        }
+        return max;
+    }
+
+    static void basic_count_sort(int[] arr) {
+        int max = findMax(arr);
+        int[] count = new int[max + 1];
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+        int k = 0;
+        for (int i = 0; i < count.length; i++) {
+            for (int j = 0; j < count[i]; j++) {
+                arr[k++] = i;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of elements");
+        int n = sc.nextInt();
+        System.out.println("Enter Elements");
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        basic_count_sort(arr);
+        display(arr);
+        sc.close();
+    }
+
+}
