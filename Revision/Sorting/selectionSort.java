7ed3e5e65ed24aa05a211bb393bab44d9b93af63
Ishita05A <ishita05agarwal@gmail.com>
2025-06-22 11:09:56 +0530

    updated code

---
+++ Revision/Sorting/selectionSort.java
@@ -0,0 +1,41 @@
+package Revision.Sorting;
+
+import java.util.Scanner;
+
+public class selectionSort {
+    static void display(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static void selection_sort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            int min_idx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min_idx]) {
+                    min_idx = j;
+                }
+            }
+            int temp = arr[i];
+            arr[i] = arr[min_idx];
+            arr[min_idx] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of elements");
+        int n = sc.nextInt();
+        System.out.println("Enter Elements");
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        selection_sort(arr);
+        display(arr);
+        sc.close();
+    }
+
+}
