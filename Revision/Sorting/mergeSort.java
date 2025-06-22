03556d7c7a1e8702e8507739755354934e6b2945
Ishita05A <ishita05agarwal@gmail.com>
2025-06-22 13:33:30 +0530

    updated code

---
+++ Revision/Sorting/mergeSort.java
@@ -0,0 +1,65 @@
+package Revision.Sorting;
+
+import java.util.Scanner;
+
+public class mergeSort {
+    static void display(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+    }
+
+    static void merge(int[] arr, int l, int r, int mid) {
+        int n1 = mid - l + 1;
+        int n2 = r - mid;
+        int[] left = new int[n1];
+        int[] right = new int[n2];
+        for (int i = 0; i < n1; i++) {
+            left[i] = arr[l + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            right[j] = arr[mid + j + 1];
+        }
+        int i = 0;
+        int j = 0;
+        int k = l;
+        while (i < n1 && j < n2) {
+            if (left[i] < right[j]) {
+                arr[k++] = left[i++];
+            } else
+                arr[k++] = right[j++];
+        }
+        while (i < n1) {
+            arr[k++] = left[i++];
+        }
+        while (j < n2) {
+            arr[k++] = right[j++];
+        }
+    }
+
+    static void merge_sort(int[] arr, int l, int r) {
+        if (l >= r)
+            return;
+        int mid = (l + r) / 2;
+        merge_sort(arr, l, mid);
+        merge_sort(arr, mid + 1, r);
+        merge(arr, l, r, mid);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of elements");
+        int n = sc.nextInt();
+        System.out.println("Enter Elements");
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        merge_sort(arr, 0, n - 1);
+        display(arr);
+        sc.close();
+    }
+
+}
