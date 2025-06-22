3751032bc88e824db13e06805f26293605995d83
Ishita05A <ishita05agarwal@gmail.com>
2025-06-22 10:43:07 +0530

    updated code

---
+++ Revision/Sorting/bubbleSort.java
@@ -0,0 +1,39 @@
+package Revision.Sorting;
+
+import java.util.Scanner;
+
+public class bubbleSort {
+    static void display(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static void bubble_Sort(int[] arr) {
+        int n = arr.length;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of elements");
+        int n = sc.nextInt();
+        System.out.println("Enter Elements");
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        bubble_Sort(arr);
+        display(arr);
+        sc.close();
+    }
+
+}
