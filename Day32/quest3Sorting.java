3ecee8d53da2c6ab3c8af6a982dce96631cda8b0
Ishita05A <ishita05agarwal@gmail.com>
2025-06-24 12:00:59 +0530

    updated code

---
+++ Day32/quest3Sorting.java
@@ -0,0 +1,43 @@
+package Day32;
+
+import java.util.Scanner;
+
+public class quest3Sorting {
+    static void display(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static void sorting(int[] arr) {
+        int x = -1;
+        int y = -1;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i - 1] > arr[i]) {
+                if (x == -1) {
+                    x = i - 1;
+                } else
+                    y = i;
+            }
+        }
+        int temp = arr[x];
+        arr[x] = arr[y];
+        arr[y] = temp;
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of elements");
+        int n = sc.nextInt();
+        System.out.println("Enter Elements");
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sorting(arr);
+        display(arr);
+        sc.close();
+    }
+
+}
