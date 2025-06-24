5af1b746c1088c63defa556beef298dd9aa920fb
Ishita05A <ishita05agarwal@gmail.com>
2025-06-25 01:07:28 +0530

    updated code

---
+++ Day32/quest5B.java
@@ -0,0 +1,52 @@
+package Day32;
+
+import java.util.Scanner;
+
+public class quest5B {
+    static void display(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+
+    static void sort(int[] arr) {
+        int n = arr.length;
+        int low = 0;
+        int mid = 0;
+        int high = n - 1;
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                swap(arr, mid, low);
+                low++;
+                mid++;
+            } else if (arr[mid] == 1) {
+                mid++;
+            } else {
+                swap(arr, mid, high);
+                high--;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of elements");
+        int n = sc.nextInt();
+        System.out.println("Enter Elements");
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        sort(arr);
+        display(arr);
+        sc.close();
+
+    }
+
+}
