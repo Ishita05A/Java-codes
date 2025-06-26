f9a9936f125ffcf0f2e6e0f436cf54e269203c8f
Ishita05A <ishita05agarwal@gmail.com>
2025-06-26 21:41:07 +0530

    updated code

---
+++ Day34/first_occurance.java
@@ -0,0 +1,39 @@
+package Day34;
+
+import java.util.Scanner;
+
+public class first_occurance {
+    static int search(int[] arr, int target) {
+        int n = arr.length;
+        int idx = -1;
+        int st = 0;
+        int end = n - 1;
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+            if (arr[mid] == target) {
+                idx = mid;
+                end = mid - 1;
+            } else if (arr[mid] > target)
+                end = mid - 1;
+            else
+                st = mid + 1;
+        }
+        return idx;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of elements");
+        int n = sc.nextInt();
+        System.out.println("Enter Elements");
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter target element");
+        int target = sc.nextInt();
+        System.out.println(search(arr, target));
+        sc.close();
+    }
+
+}
