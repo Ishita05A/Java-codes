bdafdde92155e995bc552e72bb0774f8e7572eef
Ishita05A <ishita05agarwal@gmail.com>
2025-06-30 15:45:30 +0530

    updated code

---
+++ Day38/right_middle_of_evenLL.java
@@ -0,0 +1,35 @@
+package Day38;
+
+public class right_middle_of_evenLL {
+    public static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    static int middle(Node head) {
+        Node slow = head;
+        Node fast = head;
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow.data;
+    }
+
+    public static void main(String[] args) {
+        Node a = new Node(2);
+        Node b = new Node(3);
+        Node c = new Node(4);
+        Node d = new Node(5);
+        a.next = b;
+        b.next = c;
+        c.next = d;
+        System.out.println(middle(a));
+
+    }
+
+}
