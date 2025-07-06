8996d01eb08921850f9ef61284c97563e1c2f997
Ishita05A <ishita05agarwal@gmail.com>
2025-07-06 21:12:18 +0530

    updated code

---
+++ Day44/quest1.java
@@ -0,0 +1,39 @@
+package Day44;
+
+import java.util.Scanner;
+
+public class quest1 {
+    static void display(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    static void reverse(int[] arr) {
+        int i = 0;
+        int j = arr.length - 1;
+        while (i < j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+            i++;
+            j--;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no. of elements");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter elements");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        reverse(arr);
+        display(arr);
+        sc.close();
+
+    }
+
+}
