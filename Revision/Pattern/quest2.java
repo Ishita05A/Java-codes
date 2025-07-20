bc8542642444e5e09dd0196eeb3ccb6caa65449c
Ishita05A <ishita05agarwal@gmail.com>
2025-07-20 23:01:10 +0530

    updated code

---
+++ Revision/Pattern/quest2.java
@@ -0,0 +1,25 @@
+package Revision.Pattern;
+
+import java.util.Scanner;
+
+public class quest2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter rows");
+        int n = sc.nextInt();
+        System.out.println("Enter columns");
+        int m = sc.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (j == 0 || j == m - 1 || i == 0 || i == n - 1) {
+                    System.out.print("*" + " ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+
+}
