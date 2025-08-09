182281a340ede6e52f8dc656a8bff9337071f0d7
Ishita05A <ishita05agarwal@gmail.com>
2025-08-10 01:40:41 +0530

    updated code

---
+++ Day59/swap.java
@@ -0,0 +1,20 @@
+package Day59;
+
+import java.util.Scanner;
+
+public class swap {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1st number");
+        int a = sc.nextInt();
+        System.out.println("Enter 2nd number");
+        int b = sc.nextInt();
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        System.out.println(a);
+        System.out.println(b);
+        sc.close();
+    }
+    
+}
