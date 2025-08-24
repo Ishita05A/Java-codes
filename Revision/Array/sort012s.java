cc17dbbb041b555df2eb5de3158551518868c28e
Ishita05A <ishita05agarwal@gmail.com>
2025-08-24 22:04:55 +0530

    updated code

---
+++ Revision/Array/sort012s.java
@@ -0,0 +1,50 @@
+package Revision.Array;
+
+import java.util.Scanner;
+
+public class sort012s {
+    static void display(int[] arr){
+        for(int i = 0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    static void swap(int[] arr,int i, int j){
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    } 
+    static void sort(int[] arr){
+        int n = arr.length;
+        int low = 0;
+        int mid = 0;
+        int high = n-1;
+        while(mid<=high){
+            if(arr[mid] == 0){
+                swap(arr, mid, low);
+                low++;
+                mid++;
+            }
+            else if(arr[mid] == 1) mid++;
+            else if(arr[mid] == 2){
+                swap(arr, mid, high);
+                high--;
+            }
+                        
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no of elements");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter elements");
+        for(int i = 0; i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        sort(arr);
+        display(arr);
+        sc.close();
+
+    }
+    
+}
