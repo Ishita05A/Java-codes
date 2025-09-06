efb326491edcee9295149855b9d104b430c5cc57
Ishita05A <ishita05agarwal@gmail.com>
2025-09-07 02:45:14 +0530

    updated code

---
+++ Day68/PallindromeString.java
@@ -0,0 +1,35 @@
+package Day68;
+
+import java.util.Scanner;
+
+
+public class PallindromeString {
+    static String reverseStr(String s) {
+        StringBuilder str = new StringBuilder(s);
+        String ans = "";
+        int n = str.length();
+        str.reverse();
+        for(int i = 0; i<str.length();i++){
+            StringBuilder word = new StringBuilder("");
+            
+            while(i<n && str.charAt(i) != ' '){
+                word.append(str.charAt(i));
+                i++;
+            }
+            word.reverse();
+            ans+=" "+word;
+        }
+        return ans.substring(1).toString();
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Parenthesis");
+        String str = sc.nextLine();
+        String s = reverseStr(str);
+        System.out.println(s);
+        sc.close();
+    }
+
+}
