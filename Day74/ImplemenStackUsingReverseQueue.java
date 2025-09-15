f658a8f15a2ec7ad4e7ec3bedb34171b290761cb
Ishita05A <ishita05agarwal@gmail.com>
2025-09-15 19:13:13 +0530

    updated code

--- Day74/ImplemenStackUsingReverseQueue.java
+++ Day74/ImplemenStackUsingReverseQueue.java
@@ -1,4 +1,4 @@
-package Day74;
+
 
 import java.util.*;
 
---
+++ Day75/FindInRotatedArray.java
@@ -0,0 +1,49 @@
+package Day75;
+
+import java.util.Scanner;
+
+public class FindInRotatedArray {
+    static int findIndex(int[] arr,int target){
+        int n = arr.length;
+        int idx = -1;
+        int st = 0;
+        int end = n-1;
+        while(st<=end){
+            int mid = st +(end -st)/2;
+            if(arr[mid] == target) return mid;
+            else if(arr[st] <=  arr[mid]){
+                if(target >= arr[st] && target <= arr[mid]){
+                    end = mid -1;
+                }
+                else st = mid +1;
+            }
+            else{
+                if(target>=arr[mid] && target <=arr[end]){
+                    st = mid + 1;
+                }
+                else{
+                    end = mid -1;
+                }
+            }
+        }
+        return idx;
+    
+
+
+    }
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no of elements");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter elements");
+        for(int i = 0; i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Enter target");
+        int target = sc.nextInt();
+        System.out.println(findIndex(arr, target));
+        sc.close();
+    }
+    
+}
