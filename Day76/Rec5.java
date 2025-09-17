23466158404aafbbb1d72d2e7a2030f79654614e
Ishita05A <ishita05agarwal@gmail.com>
2025-09-17 18:44:14 +0530

    updated code

---
+++ Day76/Rec5.java
@@ -0,0 +1,18 @@
+package Day76;
+
+import java.util.Scanner;
+
+public class Rec5 {
+    static int fibbo(int n){
+        if(n <= 1) return n;
+        return fibbo(n-1)+fibbo(n-2);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number");
+        int n = sc.nextInt();
+        for(int i = 0;i<n;i++) System.out.print(fibbo(i)+" ");
+        sc.close();
+    }
+    
+}
