5d5b1418765ebe2bf703e75ed8a57e6757d4d3b2
Ishita05A <ishita05agarwal@gmail.com>
2025-09-17 15:50:31 +0530

    updated code

---
+++ Day76/postFix.java
@@ -0,0 +1,56 @@
+package Day76;
+
+import java.util.Scanner;
+import java.util.*;
+
+public class postFix {
+    static String postfix(String str) {
+        Stack<String> st = new Stack<>();
+        Stack<Character> op = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            int ascii = (int) ch;
+            if (ascii >= 48 && ascii <= 57) {
+                st.push(ch + "");
+            } else if (op.size() == 0)
+                op.push(ch);
+            else if (ch == ')') {
+                while (op.peek() != '(') {
+                    String v2 = st.pop();
+                    String v1 = st.pop();
+                    char o = op.pop();
+                    st.push(v1 + v2 + o);
+                }
+                op.pop();
+            } else {
+                if (ch == '*' || ch == '/') {
+                    if (op.peek() == '*' || op.peek() == '/') {
+                        String v2 = st.pop();
+                        String v1 = st.pop();
+                        char o = op.pop();
+                        st.push(v1 + v2 + o);
+                        op.push(ch);
+                    } else
+                        op.push(ch);
+                } else
+                    op.push(ch);
+            }
+        }
+        while (op.size() > 0) {
+            String v2 = st.pop();
+            String v1 = st.pop();
+            char o = op.pop();
+            st.push(v1 + v2 + o);
+        }
+        return st.peek();
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Eter Expression");
+        String str = sc.nextLine();
+        System.out.println(postfix(str));
+        sc.close();
+    }
+
+}
