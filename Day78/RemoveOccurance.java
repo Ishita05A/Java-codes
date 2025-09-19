b496fe937eec747f500f89013ffa16ce176c620a
Ishita Agarwal <ishita05agarwal@gmail.com>
2025-09-19 22:30:08 +0530

    updated code

---
+++ Day78/RemoveOccurance.java
@@ -0,0 +1,10 @@
+package Day78;
+
+import java.util.Scanner;
+
+public class RemoveOccurance {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
+    
+}
